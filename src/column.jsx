61a677f007cf281bef5da29167041466d773bc1e
ayushchauhan1 <ayush_c@ar.iitr.ac.in>
2021-04-18 23:52:46 +0530

    first commit

---
+++ src/column.jsx
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+export default class column extends React.Component {
+  render() {
+    return this.props.column.title;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import intialData from './initial-data';
+import Column from './column';
+import '@atlaskit/css-reset';
+
+class App extends React.Component {
+  state = intialData;
+   render(){
+    return this.state.columnOrder.map((columnId) => {
+      const column = this.state.columns[columnId];
+      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+      
+      return <Column key={column.id} column={column} tasks={tasks} />;
+         
+
+     });
+
+   }
+}
+
+
+ReactDOM.render(
+    <App />  ,  document.getElementById('root')
+);
+
